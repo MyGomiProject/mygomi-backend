ddb99b9e46e41a928f549e5304f2db34e28bae17
zzozzo0427 <22000104@handong.ac.kr>
2026-02-02 10:31:10 +0900

    feat: disable spring security restrictions for development

--- src/main/java/com/mygomi/backend/security/SecurityConfig.java
+++ src/main/java/com/mygomi/backend/security/SecurityConfig.java
@@ -27,10 +27,13 @@ public class SecurityConfig {
                 .httpBasic(AbstractHttpConfigurer::disable) // HTTP Basic 끄기
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 끄기
                 .authorizeHttpRequests(auth -> auth
-                        // 1. 회원가입, 로그인, 스웨거는 누구나 접속 허용
+                        /*
+                                                // 1. 회원가입, 로그인, 스웨거는 누구나 접속 허용
                         .requestMatchers("/api/auth/**", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
                         // 2. 그 외 모든 요청은 인증 필요
                         .anyRequest().authenticated()
+                         */
+                        .anyRequest().permitAll()
                 )
                 // 3. JWT 필터 끼워넣기
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
